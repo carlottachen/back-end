d40177becb035b4ab0e918eee78133704f9ac248
Carlotta Chen <carlottachen92@gmail.com>
2021-11-29 13:03:15 -0800

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/users', (request, response) => {
+	let friends = ["Nitin", "Eric", "Jeddy", "Cameron", "Riley"];
+	response.status(200).send(friends);
+});
+
+app.get("/weather/:teamperature", (request, response) => {
+	const phrase = 
+		`<h3>It was ${request.params.temperature} today</h3>`;
+	response.status(200).send(phrase);
+
+});
+
+app.listen(4000, () => console.log("server is running on port 4000"));
